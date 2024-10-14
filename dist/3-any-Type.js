5d01f8655650c6006e6cdcfd2f03c140004a97f5
Geziel Carvalho <geziel.natal@gmail.com>
2024-10-13 22:03:12 -0400

    Remove JS files

--- dist/3-any-Type.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let password = '123456';
-password = 123456;
-password = true;
-password = [1, 2, 3, 4, 5, 6];
-console.log(password);
--- dist/4-Typescript-Exemplified.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-let grade = 100;
-let firstName = 'John';
