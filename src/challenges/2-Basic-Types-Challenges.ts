27f4086406d3b5050b2e277d2913dd5d0931cb9a
Geziel Carvalho <geziel.natal@gmail.com>
2024-10-13 22:17:03 -0400

    Basic types challenge

---
+++ src/challenges/2-Basic-Types-Challenges.ts
@@ -0,0 +1,44 @@
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Create a variable with the type number and assign it an arbitrary value
+*/
+let myNumber: number = 5;
+// ----------------------------------------------------
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// ----------------------------------------------------
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 2 ------------------
+Create a variable with the type string and use the addition operator to put two arbitrary words together
+*/
+let myString: string = 'Hello' + 'World';
+// ----------------------------------------------------
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// ----------------------------------------------------
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 3 ------------------
+Simulate a Type Error by assigning a number  to a string
+*/
+//myString = 5;
+// ----------------------------------------------------
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// ----------------------------------------------------
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 4 ------------------
+Create a variable with the type Boolean and assign it to the logical OR operator comparing two values
+*/
+let myBoolean: boolean = true || false;
+// ----------------------------------------------------
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// ----------------------------------------------------
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 5 ------------------
+Simulate a Type Error by assigning a number to a boolean
+*/
+//myBoolean = 5;
+// ----------------------------------------------------
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// ----------------------------------------------------
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 6 ------------------
+Get the length of an arbitrary string
+*/
+let myStringLength: number = myString.length;
