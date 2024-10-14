e26f07014e2f87298c35d9f8f48c2ec78da2741e
Geziel Carvalho <geziel.natal@gmail.com>
2024-10-13 22:23:00 -0400

    Add inference challenges

---
+++ src/challenges/3-Inference-Challenges.ts
@@ -0,0 +1,20 @@
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Assign a number to a variable with no type annotation, what is the variable type inferred by TypeScript?
+*/
+let mySecondNumber = 5;
+// ----------------------------------------------------
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// ----------------------------------------------------
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 2 ------------------
+Assign a string to a variable with no type annotation, what is the variable type inferred by TypeScript?
+*/
+let mySecondString = 'Five';
+// ----------------------------------------------------
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// ----------------------------------------------------
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 3 ------------------
+Assign a boolean false to a variable with no type annotation, what is the variable type inferred by TypeScript?
+*/
+let mySecondBoolean = false;
