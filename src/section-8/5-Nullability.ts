afbcafba71e42abcdc7b1da5f9a7964067a58cfc
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-10 14:01:21 -0500

    Add nullability demonstration with undefined type examples

---
+++ src/section-8/5-Nullability.ts
@@ -0,0 +1,12 @@
+let notSureOne: undefined = undefined;
+
+// Commented code will not compile
+// let notSureTwo: undefined = null;
+// let notSureThree: undefined = 123;
+// let notSureFour: undefined = '123';
+
+let notSureFive: undefined | number = 5;
+let notSureSix: undefined | number = undefined;
+
+// Commented code will not compile
+// let notSureSeven: undefined | number = null;
